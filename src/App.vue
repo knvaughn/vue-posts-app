18718f694a96b39140adaf796c22e628b18ad10c
Kristin Vaughn <dev.kristin@gmail.com>
2022-03-11 12:35:19 -0500

    Create timeline view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <section class="section">
+    <div class="container">
+      <timeline-view />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TimelineView from "./components/TimelineView.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    TimelineView,
   },
 });
 </script>
